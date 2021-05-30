a2de297c1eb504c1585220e1facca2d13fd968d9
Komari Spaghetti <3759175+Hejsil@users.noreply.github.com>
2021-05-30 22:48:08 +0200

    Use .local/share instead of .cache for feeds

--- rss.sh
+++ rss.sh
@@ -6,9 +6,9 @@ usage() {
     echo "A program for downloading all the rss feeds you follow."
     echo "$program reads \$XDG_CONFIG_HOME/rss/urls to find all the feeds you follow."
     echo "These feeds will be downloaded to \$XDG_CACHE_HOME/rss/unread. $program will"
-    echo "not download feed entries that already exists in \$XDG_CACHE_HOME/rss/{unread,read}."
+    echo 'not download feed entries that already exists in $XDG_CACHE_HOME/rss/{unread,read}.'
     echo ""
-    echo "Users can move entries between \$XDG_CACHE_HOME/rss/{unread,read} to mark whether"
+    echo 'Users can move entries between $XDG_CACHE_HOME/rss/{unread,read} to mark whether'
     ecoh "an entry has been read or not. The name of the file should not be changed though."
     echo ""
     echo "Requires: curl, sfeed"
@@ -18,9 +18,18 @@ usage() {
 
 while [ -n "$1" ]; do
     case $1 in
-        --) shift; break ;;
-        -h|--help) usage; exit 0 ;;
-        -*) usage; exit 1 ;;
+        --)
+            shift
+            break
+            ;;
+        -h | --help)
+            usage
+            exit 0
+            ;;
+        -*)
+            usage
+            exit 1
+            ;;
         *) break ;;
     esac
     shift
@@ -36,30 +45,34 @@ dir_with_fallback() {
     fi
 }
 
-global_cache_dir=$(dir_with_fallback "$XDG_CACHE_HOME" "$HOME/.cache")
+global_cache_dir=$(dir_with_fallback "$XDG_DATA_HOME" "$HOME/.local/share")
 global_config_dir=$(dir_with_fallback "$XDG_CONFIG_HOME" "$HOME/.config")
 
 # cache dirs
-cache_dir="$global_cache_dir/rss";
-unread_dir="$cache_dir/unread"; mkdir -p "$unread_dir"
-read_dir="$cache_dir/read"; mkdir -p "$read_dir"
+cache_dir="$global_cache_dir/rss"
+unread_dir="$cache_dir/unread"
+mkdir -p "$unread_dir"
+read_dir="$cache_dir/read"
+mkdir -p "$read_dir"
 
 # configs
-config_dir="$global_config_dir/rss"; mkdir -p "$config_dir"
-url_config="$config_dir/urls"; touch -a "$url_config"
+config_dir="$global_config_dir/rss"
+mkdir -p "$config_dir"
+url_config="$config_dir/urls"
+touch -a "$url_config"
 
 cut -f1 "$url_config" | xargs curl -s | sfeed | tr '\t' '\a' |
-while IFS=$(printf '\a') read -r timestamp title link content content_type id author enclosure; do
-    id=$(echo "$id" | sed 's#/#|#g')
-    [ -z "$id" ] && id=$(echo "$link" | sed 's#/#|#g')
-    [ -z "$id" ] && {
-        echo 'No id' >&2
-        continue
-    }
-    [ -e "$unread_dir/$id" ] && continue
-    [ -e "$read_dir/$id" ] && continue
+    while IFS=$(printf '\a') read -r timestamp title link content content_type id author enclosure; do
+        id=$(echo "$id" | sed 's#/#|#g')
+        [ -z "$id" ] && id=$(echo "$link" | sed 's#/#|#g')
+        [ -z "$id" ] && {
+            echo 'No id' >&2
+            continue
+        }
+        [ -e "$unread_dir/$id" ] && continue
+        [ -e "$read_dir/$id" ] && continue
 
-    printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n' "$timestamp" "$title" "$link" \
-        "$content" "$content_type" "$author" "$enclosure" \
-        >"$unread_dir/$id"
-done
+        printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n' "$timestamp" "$title" "$link" \
+            "$content" "$content_type" "$author" "$enclosure" \
+            >"$unread_dir/$id"
+    done
